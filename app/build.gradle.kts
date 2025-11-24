336f800f6ca4f2d0f750d4a9afc50ab9db2967c9
erayerarslan <eraslaneray2001@gmail.com>
2025-11-24 23:02:45 +0300

    gradle update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,9 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.daggerHilt)
+    alias(libs.plugins.org.jetbrains.kotlin.kapt)
+    id("androidx.navigation.safeargs.kotlin")
 }
 
 android {
@@ -33,6 +36,11 @@ android {
     kotlinOptions {
         jvmTarget = "11"
     }
+    buildFeatures {
+        viewBinding = true
+        buildConfig = true
+
+    }
 }
 
 dependencies {
@@ -45,4 +53,47 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+
+    implementation(libs.hilt.android)
+    kapt(libs.hilt.android.compiler)
+
+    implementation ("androidx.activity:activity-ktx:1.10.1")
+
+    // Retrofit
+    implementation ("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation ("com.squareup.retrofit2:converter-moshi:2.9.0")
+    implementation("com.squareup.moshi:moshi:1.15.0")
+    implementation("com.squareup.moshi:moshi-kotlin:1.15.0")
+    kapt("com.squareup.moshi:moshi-kotlin-codegen:1.15.0")
+    // OkHttp (isteğe bağlı)
+    implementation ("com.squareup.okhttp3:okhttp:4.9.3")
+    implementation ("com.squareup.okhttp3:logging-interceptor:4.9.3")
+    // (Opsiyonel) Navigation
+    implementation ("androidx.navigation:navigation-fragment-ktx:2.8.9")
+    implementation ("androidx.navigation:navigation-ui-ktx:2.8.9")
+
+    // RecyclerView
+    implementation ("androidx.recyclerview:recyclerview:1.4.0")
+    implementation ("com.google.android.material:material:1.12.0") // En güncel sürüm olabilir
+
+    implementation ("com.github.bumptech.glide:glide:4.16.0")
+    kapt ("com.github.bumptech.glide:compiler:4.16.0")
+
+    implementation ("io.github.afreakyelf:Pdf-Viewer:2.3.2")
+
+    implementation ("com.airbnb.android:lottie:6.1.0")
+
+    implementation ("com.facebook.shimmer:shimmer:0.5.0")
+
+    implementation ("com.jakewharton.timber:timber:5.0.1")
+
+    implementation ("com.jakewharton.threetenabp:threetenabp:1.4.5")
+
+
+}
+
+kapt {
+    correctErrorTypes = true
 }
--- build.gradle.kts
+++ build.gradle.kts
@@ -2,4 +2,23 @@
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
+
+    alias(libs.plugins.daggerHilt) apply false
+    alias(libs.plugins.org.jetbrains.kotlin.kapt) apply false
+    alias(libs.plugins.google.gms.google.services) apply false
+    alias(libs.plugins.google.firebase.crashlytics) apply false
+}
+
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:8.9.1")
+        classpath("com.google.dagger:hilt-android-gradle-plugin:2.44")
+        classpath ("androidx.navigation:navigation-safe-args-gradle-plugin:2.5.3")
+
+        // Diğer classpath'ler (AGP, Kotlin vb.)
+    }
 }
